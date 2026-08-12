3ab0afdb95d698410426c499f74fc4499b6e42a7
Tejaswi <tejaswik0755@gmail.com>
2026-08-12 14:51:37 +0530

    Completed exercises 1.1-1.5

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,12 @@
+import Part from "./Part"
+const Content= (props)=>{
+    return(
+        <>
+        <Part part= {props.parts[0]} />
+        <Part part= {props.parts[1]} />
+        <Part part= {props.parts[2]} />
+        </>
+
+    )
+}
+export default Content
---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,8 @@
+const Part=(props)=>{
+    return(
+        <p>
+            {props.part.name} {props.part.exercises}
+        </p>
+    )
+}
+export default Part 
